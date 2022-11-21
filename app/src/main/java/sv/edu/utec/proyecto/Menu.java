1b4ada3fa7f5a286b405389b168377316eb5c5bb
Julio22G <118698796+Julio22G@users.noreply.github.com>
2022-11-20 22:21:35 -0600

    Titulo: Proyecto 1 Descripcion: se agrega la actividad de levantar la activiti de camisas desde el menu Autor; JG

--- app/src/main/java/sv/edu/utec/proyecto/Menu.java
+++ app/src/main/java/sv/edu/utec/proyecto/Menu.java
@@ -2,13 +2,48 @@ package sv.edu.utec.proyecto;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class Menu extends AppCompatActivity {
 
+    ImageButton btncamisas,btnPantalones,btnshort,btnzapatos;
+    Button btnSalir;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+
+        btncamisas = findViewById(R.id.btnCamisas);
+        btnshort = findViewById(R.id.btnShort);
+        btnzapatos = findViewById(R.id.btnZapatos);
+        btnPantalones = findViewById(R.id.btnPantalones);
+        btnSalir = findViewById(R.id.btnSalir);
+
+        btncamisas.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent men = new Intent(getApplicationContext(),camisas.class);
+                startActivity(men);
+            }
+        });
+
+        btnshort.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent men = new Intent(getApplicationContext(),Short.class);
+                startActivity(men);
+            }
+        });
+        btnSalir.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent men = new Intent(getApplicationContext(),Main_login.class);
+                startActivity(men);
+            }
+        });
     }
 }
